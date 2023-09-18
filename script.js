f4d8143f2f9ebe057597217524dd2d1c7c8559ef
Nathan Ateng <nathanateng@gmail.com>
2023-09-18 15:20:54 +0300

    fix "DOM display"

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let totalScores = { computerScore: 0, playerScore: 0 };
 // create "getcomputerChoice function"
 // Does: randomly selects between rock, papers and scissors and returns what computer selects
 function getComputerChoice() {
@@ -31,10 +32,32 @@ function getResult(playerChoice, computerChoice) {
   return score;
 }
 
+// create showResult function
+// Does: updates the dom
+function showResult(score, playerChoice, computerChoice) {
+  // select result div to display in the dom
+  const resultDiv = document.getElementById("result");
+  const handsDiv = document.getElementById("hands");
+  const playerScoreDiv = document.getElementById("player-score");
+
+  // update the dom to show choice of player and computer
+  handsDiv.innerText = `${playerChoice} Vs ${computerChoice}`;
+  playerScoreDiv.innerText = `Your score is: ${totalScores["playerScore"]}`;
+  if (score == -1) {
+    resultDiv.innerText = "You Lost";
+  } else if (score == 0) {
+    resultDiv.innerText = "It's a tie";
+  } else {
+    resultDiv.innerText = "You win!";
+  }
+}
 // creat playGame function
 //Does: allws usser to play game
 function onclickRPS(playerChoice) {
-  console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const score = getResult(playerChoice, computerChoice);
+  totalScores["playerScore"] += score;
+  showResult(score, playerChoice, computerChoice);
 }
 function playGame() {
   // use query selector to select all buttons
@@ -43,6 +66,21 @@ function playGame() {
   rpsButtons.forEach((rpsButton) => {
     rpsButton.onclick = () => onclickRPS(rpsButton.value);
   });
+  const endGameButton = document.getElementById("endGameButton");
+  endGameButton.onclick = () => endGame(totalScores);
 }
 
+// create ebdGame  function
+//Does: ends game and clears all text on the dom
+function endGame(totalScore) {
+  totalScores["playerScore"] = 0;
+  totalScores["computerScore"] = 0;
+  const resultDiv = document.getElementById("result");
+  const handsDiv = document.getElementById("hands");
+  const playerScoreDiv = document.getElementById("player-score");
+
+  resultDiv.innerText = "";
+  handsDiv.innerText = "";
+  playerScoreDiv.innerText = "";
+}
 playGame();
